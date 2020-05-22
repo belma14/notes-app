b9cbb1f501350cf7fdea4459b91214bccf3e1e3f
Belma Homarac <belmah@comtrade.com>
2020-05-22 11:26:55 +0200

    First commit

---
+++ src/test/java/notesapp/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package notesapp.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
